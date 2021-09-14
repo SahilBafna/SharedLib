5ac0ded9534cc0e1615ab0663b460f1d505ebd2f
SahilBafna <67066509+SahilBafna@users.noreply.github.com>
2021-09-14 13:02:47 +0530

    Update swissknife.groovy

--- vars/swissknife.groovy
+++ vars/swissknife.groovy
@@ -1,8 +1,8 @@
 #!/usr/bin/groovy
 def printName(repo) {
-  //git clone "${repo}"
+  git url: ${repo}
   //echo 'hello ${repo}'
-  ["git", "clone", "https://github.com/SahilBafna/Petclinic.git"].execute()
+  //["git", "clone", "https://github.com/SahilBafna/Petclinic.git"].execute()
 }
 //return this
 
